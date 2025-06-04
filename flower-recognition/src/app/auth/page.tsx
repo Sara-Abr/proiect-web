2473f758cb0250370e1b80ec4aed00f4294d9593
Sara-Abr <lillestcloud@duck.com>
2025-06-04 03:18:24 +0300

    .

--- flower-recognition/src/app/auth/page.tsx
+++ flower-recognition/src/app/auth/page.tsx
@@ -11,6 +11,20 @@ import {
 } from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 import { useRouter } from 'next/router';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => { //trbuie implementat in fiecare pagina pt care conteaza autentificarea
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
 export default function Auth() {
   const form = useForm({
     defaultValues: {
